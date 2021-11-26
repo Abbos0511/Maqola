74662d20de144fc8dca3ce514139fd968e763306
Abbos0511 <89196424+Abbos0511@users.noreply.github.com>
2021-11-26 17:26:57 +0500

    hi

--- Maqola/settings.py
+++ Maqola/settings.py
@@ -140,6 +140,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
